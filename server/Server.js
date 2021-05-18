163fe74a8eb1119634ca78ef09c32333c231d029
bmwasonga <bmwasonga@gmail.com>
2021-05-19 02:00:21 +0300

    added more functionality

--- server/Server.js
+++ server/Server.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 5000;
 //using the modal that we have created above
 const Socks = require('./model/socksModel');
 
+//import the Route of the socks as exported from the route folder
+const socksRoute = require('./routes/socksRoute');
+
+//rest of code
 const app = express();
 
 app.use(express.json());
@@ -14,17 +18,9 @@ app.get('/', (req, res) => {
   res.status(200).send('<h1>The server is running </h1>');
 });
 
-app.get('/getsocks', (req, res) => {
-  Socks.find({}, (err, result) => {
-    if (err) {
-      log.error(err);
-    } else {
-      res.status(200).send(result);
-    }
-  });
-});
-// the above when we run http://localhost:5000/getsocks should retun the list as extracted from MongoDB
+//to utilise the SocksRoute that has  been imported from
+app.use('/api/socks/getallsocks', socksRoute);
 
 app.listen(port, () => {
-  console.log(`the server is listening on port: ${port}`);
+  console.log(`the server is liste ning on port: ${port}`);
 });
---
+++ server/routes/socksRoute.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+//import the socks mmodel
+const socks = require('../model/socksModel');
+
+router.get('/getallsocks', async (req, res) => {
+  try {
+    const socks = await Socks.find({});
+    res.status(200).send(socks);
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
+});
+
+module.exports = router;
