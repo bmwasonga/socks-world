7a03792b8449908558c48432f4b3e6cc18f653b9
bmwasonga <bmwasonga@gmail.com>
2021-05-21 21:30:32 +0300

    more cart functions

--- client/src/components/Screens/Cart.js
+++ client/src/components/Screens/Cart.js
@@ -8,7 +8,23 @@ export default function Cart() {
     <div>
       <div className="row justify-content-center">
         <div className="col-md-6">
-          <h1> {cartItems.length}</h1>
+          <h2> My cart</h2>
+
+          {cartItems.map((item, idx) => {
+            return (
+              <div className="flex-container flex-column " key={idx}>
+                <div>
+                  {item.name} [{item.size}]
+                </div>
+                <div>
+                  price: {item.quantity} *{item.prices[0][item.size]} =
+                  {item.price}
+                </div>
+                <div></div>
+                <hr />
+              </div>
+            );
+          })}
         </div>
         <div className="col-md-4"></div>
       </div>
