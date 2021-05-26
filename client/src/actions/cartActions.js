f14f54387f438fcb3a7a1257e74a63a192501382
bmwasonga <bmwasonga@gmail.com>
2021-05-26 18:33:07 +0300

    added cart functionality

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -17,3 +17,8 @@ export const addToCart = (sock, size, quantity) => (dispatch, getState) => {
 };
 
 //the params above will be used to calculate the total ammount on checkout
+export const deleteFromCart = (sock) => (dispatch, getState) => {
+  dispatch({ type: 'DELETE_FROM_CART', payload: sock });
+  const cartItems = getState().cartReducer.cartItems;
+  sessionStorage.setItem('cartItems', JSON.stringify(cartItems));
+};
--- client/src/components/Screens/Cart.js
+++ client/src/components/Screens/Cart.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import {
+  AiOutlinePlusCircle,
+  AiOutlineMinusCircle,
+  AiFillDelete,
+} from 'react-icons/ai';
+import { addToCart, deleteFromCart } from '../../actions/cartActions';
 
 export default function Cart() {
   const cartSate = useSelector((state) => state.cartReducer);
   const cartItems = cartSate.cartItems;
+
+  //dispatch the add and reduce to cart cartActions
+  const dispatch = useDispatch();
+
   return (
     <div>
       <div className="row justify-content-center">
@@ -12,21 +22,53 @@ export default function Cart() {
 
           {cartItems.map((item, idx) => {
             return (
-              <div className="flex-container flex-column " key={idx}>
-                <div>
-                  {item.name} [{item.size}]
+              <div className="flex-container" key={idx}>
+                <div className=" m-1 w-100 ">
+                  <h5>
+                    {item.name} [{item.size}]
+                  </h5>
+                  <h5>
+                    price: {item.quantity} *{item.prices[0][item.size]} =
+                    {item.price}
+                  </h5>
+                  <h5>
+                    Quantity:
+                    <AiOutlinePlusCircle
+                      color="green"
+                      onClick={() => {
+                        dispatch(addToCart(item, item.size, item.quantity + 1));
+                      }}
+                    />
+                    <b> {item.quantity} </b>
+                    <AiOutlineMinusCircle
+                      color="red"
+                      onClick={() => {
+                        dispatch(addToCart(item, item.size, item.quantity - 1));
+                      }}
+                    />
+                  </h5>
+                  <hr />
                 </div>
-                <div>
-                  price: {item.quantity} *{item.prices[0][item.size]} =
-                  {item.price}
+                <div className="m-1 ">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    style={{ height: '80px', width: '80px' }}
+                  />
                 </div>
-                <div></div>
-                <hr />
+                <div className="w-10 mt-4">
+                  <AiFillDelete
+                    color="red"
+                    onClick={() => {
+                      dispatch(deleteFromCart(item));
+                    }}
+                  />
+                </div>
               </div>
             );
           })}
         </div>
-        <div className="col-md-4"></div>
+        <div className="">this should appear on the side</div>
       </div>
     </div>
   );
