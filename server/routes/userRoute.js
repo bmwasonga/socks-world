97ac27db01f792c8478eadcec1caa662e3abacc0
bmwasonga <bmwasonga@gmail.com>
2021-06-07 10:30:30 +0300

    login backend

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -15,4 +15,43 @@ router.post('/register', async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.find({ email, password });
+    if (user.length > 0) {
+      const currentUser = {
+        name: user[0].name,
+        email: user[0].email,
+        isAdmin: user[0].isAdmin,
+        _id: user[0]._id,
+      };
+      res.send(currentUser);
+    } else {
+      return res.status(400).json({ message: 'User Login failed' });
+    }
+  } catch (error) {
+    return res.status(400).json({ message: 'Something went wrong' });
+  }
+});
+
+router.get('/getallusers', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status.send(400).json({ message: 'Something went wrong' });
+  }
+});
+
+router.post('/deleteUser', async (req, res) => {
+  const userID = req.body.userID;
+  try {
+    await User.findOneAndDelete({ _id: userID });
+    res.send('The user has been deleted');
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
+});
 module.exports = router;
