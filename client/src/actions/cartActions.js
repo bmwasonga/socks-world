ad3ec09c4bd2c7a7ac996724dac0ce211119b65c
bmwasonga <bmwasonga@gmail.com>
2024-11-08 15:44:35 +0300

    another random add

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -9,24 +9,24 @@ export const addToCart = (sock, size, quantity) => (dispatch, getState) => {
     price: sock.prices[0][size] * quantity,
   };
 
-  //alert fro more than 10 iems and
+  //alert for more than 10 iems and
   if (cartItem.quantity > 10) {
-    alert('You cannot buy more than 10 pieces of an item');
+    alert("You cannot buy more than 10 pieces of an item");
   } else if (cartItem.quantity <= 0) {
-    dispatch({ type: 'DELETE_FROM_CART', payload: sock });
+    dispatch({ type: "DELETE_FROM_CART", payload: sock });
   } else {
-    dispatch({ type: 'ADD_TO_CART', payload: cartItem });
+    dispatch({ type: "ADD_TO_CART", payload: cartItem });
   }
 
   const cartItems = getState().cartReducer.cartItems;
-  sessionStorage.setItem('cartItems', JSON.stringify(cartItems));
+  sessionStorage.setItem("cartItems", JSON.stringify(cartItems));
 };
 
 //the params above will be used to calculate the total ammount on checkout
 export const deleteFromCart = (sock) => (dispatch, getState) => {
-  dispatch({ type: 'DELETE_FROM_CART', payload: sock });
+  dispatch({ type: "DELETE_FROM_CART", payload: sock });
   const cartItems = getState().cartReducer.cartItems;
-  sessionStorage.setItem('cartItems', JSON.stringify(cartItems));
+  sessionStorage.setItem("cartItems", JSON.stringify(cartItems));
 };
 
 //the above will bw used to delete items entirely from the cart
